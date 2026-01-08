4981f1a4f73da001f29166c743bd6abfa63b9df3
weeloser <germankokotkin@mail.ru>
2026-01-08 21:02:40 +0300

    update
    Added CORS middleware and improved IP handling for connections.

--- web/main.py
+++ web/main.py
@@ -2,23 +2,30 @@ import socketio
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 import time
 from collections import defaultdict
 
-# Создаем сервер
-sio = socketio.AsyncServer(async_mode='asgi', cors_allowed_origins='*')
+# Настройка сервера
+sio = socketio.AsyncServer(async_mode='asgi', cors_allowed_origins='*', ping_timeout=60)
 app = FastAPI()
 socket_app = socketio.ASGIApp(sio, app)
 
-# Шаблоны (HTML)
+# Разрешаем CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 templates = Jinja2Templates(directory="templates")
 
-# Хранилище данных (в памяти)
-# rooms = { room_id: { sid: { name, avatar, is_admin, ip } } }
+# Структура данных: room_id -> sid -> user_info
 rooms = defaultdict(dict)
-# banned_ips = { ip: timestamp_until_unban }
+# Баны: ip -> timestamp
 banned_ips = {}
 
 @app.get("/")
@@ -29,22 +36,28 @@ async def index(request: Request):
 async def room(request: Request, room_id: str):
     return templates.TemplateResponse("index.html", {"request": request})
 
-# --- Логика Socket.IO ---
-
 @sio.event
 async def connect(sid, environ):
-    # Получаем IP клиента (на Render он может быть в заголовке x-forwarded-for)
+    # Получаем реальный IP через заголовки прокси (для Render)
     headers = dict(environ.get('asgi.scope', {}).get('headers', []))
-    # Простая логика извлечения IP для примера
-    client_ip = environ.get('REMOTE_ADDR') 
+    # x-forwarded-for обычно содержит реальный IP на Render
+    x_forwarded_for = None
+    for k, v in headers.items():
+        if k == b'x-forwarded-for':
+            x_forwarded_for = v.decode()
+            break
+            
+    client_ip = x_forwarded_for if x_forwarded_for else environ.get('REMOTE_ADDR')
     
-    # Проверка бана
     current_time = time.time()
     if client_ip in banned_ips:
         if current_time < banned_ips[client_ip]:
-            return False # Отклонить соединение
+            return False 
         else:
-            del banned_ips[client_ip] # Бан истек
+            del banned_ips[client_ip]
+
+    # Сохраняем IP в сессии сокета для использования при бане
+    await sio.save_session(sid, {'ip': client_ip})
 
 @sio.event
 async def join_room(sid, data):
@@ -52,82 +65,89 @@ async def join_room(sid, data):
     user_name = data['name']
     avatar = data['avatar']
     
-    # Получаем IP (упрощенно для примера)
-    client_ip = "user_ip" 
+    # Получаем сессию для IP
+    session = await sio.get_session(sid)
+    client_ip = session.get('ip', 'unknown')
 
-    # Если комната пуста, первый вошедший - админ
     is_admin = len(rooms[room_id]) == 0
     
-    rooms[room_id][sid] = {
+    user_info = {
         'name': user_name,
         'avatar': avatar,
         'is_admin': is_admin,
-        'ip': client_ip
+        'ip': client_ip,
+        'video_enabled': data.get('video_enabled', False),
+        'audio_enabled': data.get('audio_enabled', False)
     }
+    
+    rooms[room_id][sid] = user_info
     
     await sio.enter_room(sid, room_id)
     
-    # Сообщаем всем, кто в комнате, что пришел новый участник
+    # Уведомляем остальных
     await sio.emit('user_joined', {
         'sid': sid,
-        'name': user_name,
-        'avatar': avatar,
-        'is_admin': is_admin
+        **user_info
     }, room=room_id, skip_sid=sid)
     
-    # Отправляем новому участнику список тех, кто уже там
+    # Отправляем список существующих пользователей новому
     existing_users = []
     for existing_sid, info in rooms[room_id].items():
         if existing_sid != sid:
-            existing_users.append({
-                'sid': existing_sid,
-                'name': info['name'],
-                'avatar': info['avatar'],
-                'is_admin': info['is_admin']
-            })
+            existing_users.append({'sid': existing_sid, **info})
     
     await sio.emit('existing_users', existing_users, to=sid)
     
-    # Если ты админ, скажем об этом
     if is_admin:
         await sio.emit('set_admin', {'is_admin': True}, to=sid)
 
 @sio.event
 async def signal(sid, data):
-    # Пересылка WebRTC сигналов (offer, answer, candidate) конкретному пользователю
     target_sid = data['target']
-    await sio.emit('signal', {
-        'sender': sid,
-        'type': data['type'],
-        'data': data['data']
-    }, to=target_sid)
+    if target_sid in rooms.get(data.get('room', ''), {}): # Простая проверка существования
+         await sio.emit('signal', {
+            'sender': sid,
+            'type': data['type'],
+            'data': data['data']
+        }, to=target_sid)
+    elif target_sid in [k for r in rooms.values() for k in r]: # Fallback поиск по всем
+        await sio.emit('signal', {
+            'sender': sid,
+            'type': data['type'],
+            'data': data['data']
+        }, to=target_sid)
+
+@sio.event
+async def state_change(sid, data):
+    # Обновление состояния (камера/микрофон вкл/выкл) для отображения заглушек у других
+    room_id = data['room']
+    if sid in rooms[room_id]:
+        rooms[room_id][sid]['video_enabled'] = data.get('video', False)
+        rooms[room_id][sid]['audio_enabled'] = data.get('audio', False)
+        await sio.emit('user_state_changed', {
+            'sid': sid,
+            'video': data.get('video'),
+            'audio': data.get('audio')
+        }, room=room_id, skip_sid=sid)
 
 @sio.event
 async def reaction(sid, data):
     room_id = data['room']
-    emoji = data['emoji']
-    await sio.emit('show_reaction', {'sid': sid, 'emoji': emoji}, room=room_id)
+    await sio.emit('show_reaction', {'sid': sid, 'emoji': data['emoji']}, room=room_id)
 
 @sio.event
 async def kick_user(sid, data):
     room_id = data['room']
     target_sid = data['target_sid']
     
-    # Проверка прав
     if rooms[room_id].get(sid, {}).get('is_admin'):
-        # Бан на 3 минуты (180 сек)
-        target_ip = rooms[room_id].get(target_sid, {}).get('ip')
-        if target_ip:
-            banned_ips[target_ip] = time.time() + 180
+        target_info = rooms[room_id].get(target_sid)
+        if target_info:
+            target_ip = target_info['ip']
+            banned_ips[target_ip] = time.time() + 180 # 3 минуты
             
-        await sio.emit('kicked', {}, to=target_sid)
-        await sio.disconnect(target_sid)
-        
-        # Удаляем из списка
-        if target_sid in rooms[room_id]:
-            del rooms[room_id][target_sid]
-            
-        await sio.emit('user_left', {'sid': target_sid}, room=room_id)
+            await sio.emit('kicked', {}, to=target_sid)
+            await sio.disconnect(target_sid)
 
 @sio.event
 async def disconnect(sid):
@@ -135,10 +155,9 @@ async def disconnect(sid):
         if sid in rooms[room_id]:
             del rooms[room_id][sid]
             await sio.emit('user_left', {'sid': sid}, room=room_id)
-            # Если комната пуста, удаляем её запись
             if not rooms[room_id]:
                 del rooms[room_id]
             break
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:socket_app", host="0.0.0.0", port=8000, reload=True)
